b1e8205d4d4dcdc4008bef3b24dc447e9eda77aa
Wealth Iduwe <wealthiduwe@gmail.com>
2021-08-05 04:05:27 +0100

    tailwind with html and css projects

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+      Quicksand: ["Quicksand", "serif"],
+      Caudex: ["Caudex", "serif"],
+      Lato: ["Lato", "serif"],
+      // Ensure fonts with spaces have " " surrounding it.
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
